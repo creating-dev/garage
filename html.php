3e1767a407b701c7fcdb0d7e61ee957d37057c72
Ménétrier Jérôme <menetrier.jerome@hotmail.com>
2018-06-24 19:55:44 +0200

    essais
    créa et essais html / css

---
+++ html.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>model Laravel</title>
+
+    <!-- Styles -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Tangerine|Ewert|Rye|Vast+Shadow" rel="stylesheet">
+</head>
+<body>
+<h1>loc1pont</h1>
+
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script type="application/javascript" src="js/app.js"></script>
+</body>
+</html>
